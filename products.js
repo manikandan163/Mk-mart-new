fab9a3625ec127c60d4f45cbfa87f44f8372c6ba
Mani Kandan <manikandanthb@gmail.com>
2026-06-02 15:56:25 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,10 @@
+let products = [
+{id:1,name:"iPhone 15",price:79999,cat:"Electronics",img:"https://images.unsplash.com/photo-1695048133142-1f4b8a4c7c88?w=500"},
+{id:2,name:"Nike Shoes",price:2999,cat:"Fashion",img:"https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=500"},
+{id:3,name:"Smart Watch",price:3499,cat:"Electronics",img:"https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500"},
+{id:4,name:"Lipstick",price:599,cat:"Beauty",img:"https://images.unsplash.com/photo-1586495777744-4413f21062fa?w=500"},
+{id:5,name:"Chair",price:1200,cat:"Home",img:"https://images.unsplash.com/photo-1582582494700-7e6f4a8b2b9d?w=500"},
+{id:6,name:"Rice Bag",price:999,cat:"Grocery",img:"https://images.unsplash.com/photo-1604908176997-125f25cc500f?w=500"},
+{id:7,name:"Headphones",price:1499,cat:"Electronics",img:"https://images.unsplash.com/photo-1585386959984-a41552231693?w=500"},
+{id:8,name:"Football",price:799,cat:"Sports",img:"https://images.unsplash.com/photo-1575361204480-aadea25e6e68?w=500"},
+];
